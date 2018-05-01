23dd52c207e7bcd2ec4ae4d03907e2bcbf862f12
Anjan Momi <anjan@momi.ca>
2018-05-01 00:45:26 -0700

    added write msg to disk

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import argparse
+import os
+from os import listdir
+from os.path import isfile, join
+
+EXIT_MSG = "Exiting. Remember to run daemon mode (with -d) to send your message."
+WAITING_MSG_FOLDER = './waiting/'
+
+def writeMsgToDisk(content):
+    onlyfiles = [f for f in listdir(WAITING_MSG_FOLDER) if isfile(join(WAITING_MSG_FOLDER, f))]
+    newFileName = str(len(onlyfiles))
+
+    with open(os.path.join(WAITING_MSG_FOLDER, newFileName + '.txt'), 'a') as new_mail:
+        for line in content:
+            new_mail.write(line)
+
+    print(EXIT_MSG)
 
 def madlib(template):
-    print("Creating new message from " + template + "\n")
-    print("Club mailer will print out a line that has a blank\n")
-    print("Type what you want the blank to be filled with and press ENTER\n")
-    print("If a line has multiple blanks, type what you would like in the first\n")
-    print("blank, press enter, type what you would like in the next blank  press enter, etc.\n")
+    print("Creating new message from " + template)
+    print("Club mailer will print out a line that has a blank")
+    print("Type what you want the blank to be filled with and press ENTER")
+    print("If a line has multiple blanks, type what you would like in the first")
+    print("blank, press enter, type what you would like in the next blank  press enter, etc.")
 
     PLACE_HOLDER = "{}"
 
@@ -24,7 +40,21 @@ def madlib(template):
                 print(line)
                 content[contentIdx] = line
 
-        print(content)
+    print("Your message is:")
+    for line in content:
+        print(line)
+
+    writeOrNo = input("Write to message? [Y/n]")
+
+    # NOTE: Check this
+    while(writeOrNo != "Y" and writeOrNo != "n"):
+        print("Please choose Y or n")
+        writeOrNo = input("Write to message? [Y/n]")
+
+    if writeOrNo == "Y":
+        writeMsgToDisk(content)
+    elif writeOrNo == "n":
+        print(EXIT_MSG)
 
 if __name__ == "__main__":
     print("Welcome to club mailer!")
